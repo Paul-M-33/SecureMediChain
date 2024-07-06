086b68f05afd63428f1f46b1171f7b5fb1b4955f
Paul Merlin <polo.m.33@hotmail.fr>
2024-07-06 18:10:07 +0200

    contract v1, manage data access depending on roles

---
+++ backend/contracts/SecureMediChain.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title SecureMediChain
+ * @dev Store patient prescription data. They can be managed depending on different roles (doctor or pharamacist).
+ */
+contract DataMediChain is Ownable {
+
+    struct PrescriptionData {
+        bytes32 prescriptionHash;
+        bool hasBeenProcessed; // replace with a counter to manage reutilisable prescriptions ?
+        bool prescriptionExist;
+    }
+
+    mapping (address => PrescriptionData) public prescriptions;
+
+    mapping (address => bool) public doctors;
+
+    mapping (address => bool) public pharmacists;
+
+    constructor() Ownable(msg.sender) {
+    }
+
+    modifier onlyDoctors() {
+        require(doctors[msg.sender], "You're not a doctor");
+        _;
+    }
+
+    modifier onlyPharmacists() {
+        require(pharmacists[msg.sender], "You're not a pharmacist");
+        _;
+    }
+
+    modifier onlyDoctorsOrPharmacists() {
+        require(doctors[msg.sender] || pharmacists[msg.sender], "You're neither a doctor nor a pharmacist");
+        _;
+    }
+
+    function addDoctor(address _doctorAddress) external onlyOwner {
+        doctors[_doctorAddress] = true;
+    }
+
+    function removeDoctor(address _doctorAddress) external onlyOwner {
+        doctors[_doctorAddress] = false;
+    }
+
+    function addPharmacist(address _pharmacistAddress) external onlyOwner {
+        pharmacists[_pharmacistAddress] = true;
+    }
+
+    function removePharmacist(address _pharmacistAddress) external onlyOwner {
+        pharmacists[_pharmacistAddress] = false;
+    }
+
+    function createNewPrescriptionData(address _patientAddress, bytes32 _prescriptionHash) external onlyDoctors {
+        PrescriptionData memory prescriptionData;
+        prescriptionData.prescriptionHash = _prescriptionHash;
+        prescriptionData.hasBeenProcessed = false;
+        prescriptionData.prescriptionExist = true;
+        prescriptions[_patientAddress] = prescriptionData;
+    }
+
+    function getPrescriptionData(address _patientAddress) external onlyDoctorsOrPharmacists view returns (PrescriptionData memory) {
+        require(prescriptions[_patientAddress].prescriptionExist, "This prescription does not exist");
+        return prescriptions[_patientAddress];
+    }
+
+    function setPrescriptionAsProcessed(address _patientAddress) external onlyPharmacists {
+        require(prescriptions[_patientAddress].prescriptionExist, "This prescription does not exist");
+        require(!prescriptions[_patientAddress].hasBeenProcessed, "This prescription has already been processed");
+        prescriptions[_patientAddress].hasBeenProcessed = true;
+    }
+}
+
+/* TODO : data accessibility ? private ? */
+/* TODO : manage multiple prescriptions for a given patient ? array of prescriptions ? */
+/* TODO : also store signature in BC ? */
+/* TODO : store a private data instead of the public key to avoid identity usurpation ? */
